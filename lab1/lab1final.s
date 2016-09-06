d27dd76820b3a20e3abc5109debfdedb89f6981a
Taylor Gregg Cowley <tcowley0@batwing.ee.byu.edu>
2016-09-06 17:54:28 -0600

    made it so it does a stack frame. Passed off lab1

--- lab1/lab1final.s
+++ lab1/lab1final.s
@@ -1129,7 +1129,7 @@ L_clib_83:
 ; bp +2 = return address
 ; bp +4 = int a
 ; bp +6 = char b (bp + 7 is empty)
-; bp +8 = char c (bp + 8 is empty)
+; bp +8 = char c (bp + 9 is empty)
 ; bp +10 = int d
 ; bp +12 = int e
 
@@ -1149,44 +1149,30 @@ L_clib_83:
 ; cx = counter for loops
 ; dx = data, general, division remainder
 AsmFunction:
+	push bp
+	mov bp, sp
 	push bx ; gotta save bx
 	push dx ; this gets used for multiplication and division
+
 	mov dx, 0	
 	
-	;What I thought it should be based on documentation
-		;	mov		al, byte [bp+6]	; Put b into ax
-	
-	mov		al, byte [bp-6] ; actually put c into al
+	mov		al, byte [bp+8] ; actually put c into al
 	cbw ; sign-extend c
 	mov		bx, ax ; put c into bx now
 
-	mov		al, byte [bp-8] ; actually put b into ax
+	mov		al, byte [bp+6] ; actually put b into ax
 	cbw	; sign-extend al to ax.
 
-	;What I thought it should be based on documentation
-		;	mov	bl, byte[bp+8]	; convert c to a word?
-	;Testing that a is actually in bp-10
-		;mov cx, word [bp-10] ;just to check that this is actually a.
-
 	add		ax, bx ; b = b+c
-	imul		word [bp-10] ; b = b*a
-	; now our value is DX:AX = b*a
-	; This is a 32-bit value. And we have to treat it as such.
-	; which the div operation already does! YES!
-	;
-	;;;;;;;;;;;;;;;;;;;;;
-	; THIS IS A HACK.
-	; For the 3rd call, we have Ax = -103 and what is stored in bp-10 is 17.
-	; Multiplying those we get Ax = -1751 as expected,
-	; but DX changes to 16 for some reason >:(
-	; It REALLY should detect that AX is negative and go negative itself (0xFFFF)
+	imul		word [bp+4] ; b = b*a
 
-	mov		bx, word [bp-4] ; Put d into bx
-	sub		bx, word [bp-2] ; d = d-e
+	mov		bx, word [bp+10] ; Put d into bx
+	sub		bx, word [bp+12] ; d = d-e
 	idiv		bx	; ax = dx:ax / bx
 	add		ax, [gvar] ; apparently I can access gvar directly?
 	pop dx	; this one got clobbered in multiplication
 	pop bx	; gotta unsave bx
+	pop bp
 	; we DIDN'T save ax because it is the return register
 	ret
 
