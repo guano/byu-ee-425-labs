80a986d2f5b82ceb96c23ffb95ca8578c64c95d2
Taylor Gregg Cowley <tcowley0@licorice.ee.byu.edu>
2016-10-05 14:45:15 -0600

    added some comments for yakk.h and yaku.h

---
+++ Lab/lab4/yakk.h
@@ -0,0 +1,17 @@
+#ifndef YAKK_H
+#define YAKK_H
+
+// A .h file for kernel code, not modified by the user.
+// It should include declarations such as TCB, YKSEM, and YKW,
+// as well as prototypes for the kernel functions.
+// Global variables shared by kernel and application code should be declared
+// as extern in this file
+
+
+
+
+// TODO: write all the declarations for the kernel functions in here
+
+
+#endif // YAKK_H
+
